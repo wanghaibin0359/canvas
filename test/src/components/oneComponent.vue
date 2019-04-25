c8aba8a036927a7623119a1b80f5e74d4121b9c5
wanghaibin <wanghb@lab.rhzz.com.cn>
2019-04-25 14:13:07 +0800

    修改拖拽功能

--- test/src/components/oneComponent.vue
+++ test/src/components/oneComponent.vue
@@ -20,54 +20,111 @@
     this.cheight = '',
     this.cloneEle=''
     this.preIndex=''
-    
+    this.targetEle=''
+    this.bindmousedown = this.onMosedown.bind(this)
+    this.bindmousemove = this.onMosemove.bind(this)
+    this.bindmouseup = this.onMoseUp.bind(this)
+    this.bindmouseover = this.targetElemousemove.bind(this)
   }
   drop.prototype={
     init () {
-      console.log(this.ele)
-      this.ele.addEventListener('mousedown',this.onMosedown.bind(this))
-      document.addEventListener('mousemove',this.onMosemove.bind(this))
-       this.ele.addEventListener('mouseup',this.onMoseUp.bind(this))
+      this.ele.addEventListener('mousedown',this.bindmousedown)
+      this.ele.addEventListener('mousemove',this.bindmouseover)
+      this.ele.addEventListener('mouseup',this.bindmouseup)
     },
     destory(){
 
     },
     onMosedown (e) {
-      console.log('down')
-      console.log(e)
       let target = e.target;
+     if(! Array.from(this.children).some(ele=>{
+        return ele==target
+        
+      })){
+        return
+      }
       this.cloneEle = target.cloneNode(true) ;
-       document.body.appendChild(this.cloneEle)
+       this.ele.appendChild(this.cloneEle)
       //找到鼠标坐标x,y,当前元素宽和高
        this.cloneEle.style.backgroundColor='green'
-      this.cloneEle.style.position='fixed';
+      this.cloneEle.style.position='absolute';
+      this.cloneEle.style.opacity='0.6'
        this.cwidth=e.offsetX, this.cheight=e.offsetY;
       let screenX =e.pageX,screenY =e.pageY;
       this.x = screenX- this.cwidth
       this.y = screenY- this.cheight
-      console.log(  this.x, this.y)
       this.cloneEle.style.left=this.x+"px"
       this.cloneEle.style.top=this.y+"px"
         this.cloneEle.style.margin=0+"px"
+        this.cloneEle.style.pointerEvent='none'
+         document.addEventListener('mousemove',this.bindmousemove)
     },
     onMosemove(e){
-      console.log(e)
       if( !this.cloneEle){return}
-     
       this.x = e.x- this.cwidth;
       this.y = e.y-this.cheight;
+     let targetEle = GetDomByPosition(this.x,this.y)
+    
       this.cloneEle.style.left=this.x+"px"
       this.cloneEle.style.top=this.y+"px"
     },
     onMoseUp(e){
+       if( !this.cloneEle){return}
       console.log('up')
-      console.log(e)
       
+       document.removeEventListener('mousemove',this.bindmousemove)
+       this.ele.removeChild(this.cloneEle)
+       console.log(this.cloneEle)
       this.cloneEle = null
     },
+    targetElemousemove(e){
+       if( !this.cloneEle){return}
+      
+       console.log(e.target)
+    }
     
   }
+function GetDomByPosition(x,y) {
+    var sx =document.documentElement.scrollLeft;
+    var sy =document.documentElement.scrollTop;
+    var  dom =  document.elementFromPoint(x-sx, y-sy) ;
 
+    if (dom == null) {
+        return null;
+    }
+
+    var $dom =$(dom);
+    var DomName = $dom[0].tagName.toLowerCase();
+
+    if (DomName == 'html' || DomName == 'body') {
+        return null;
+    }
+
+    while (DomName == 'iframe' || DomName == 'frame')
+    {
+
+        var FLeft = Math.ceil($dom.offset().left);
+        var FTop  = Math.ceil($dom.offset().top);
+        x = x - FLeft;
+        y = y - FTop;
+        // alert ('坐标:'+ x+'*'+y );
+        var $dom=$($dom[0].contentWindow.document.elementFromPoint(x, y));
+
+        if ($dom[0]== null) {
+            return null;
+        }
+
+        DomName = $dom[0].tagName.toLowerCase();
+
+        if (DomName == 'html' || DomName == 'body') {
+            return null;
+        }
+
+    }
+
+    return $dom;
+
+}
 
 
   export default {
